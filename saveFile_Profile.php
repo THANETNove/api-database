67e4c1c257d7479c209517eb4664b33b113d6581
THANETNove <Novel_Manga@outlook.co.th>
2022-07-16 22:51:11 +0700

    เพิ่ม ภาพ profile

---
+++ saveFile_Profile.php
@@ -0,0 +1,55 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+
+header("content-type:text/javascript;charset=utf-8");
+error_reporting(0);
+error_reporting(E_ERROR | E_PARSE);
+$link = mysqli_connect('localhost', 'root', '', "app_tradesman");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    
+    exit;
+}
+
+if (!$link->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $link->error);
+    exit();
+	}
+
+
+ $file = time(). "_" . basename($_FILES['image']['name']);
+
+ $tmp_name = $_FILES['image']['tmp_name'];
+ $id_name = $_POST['id_user'];
+if(move_uploaded_file($tmp_name,"images/profile/".$file)){
+
+    $sql = "INSERT INTO `image_profile`(`id`,`id_user`, `file_src`) VALUES (Null,'$id_name', '$file')";
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        
+        /* return */echo $result;
+    } else {
+        /* return */echo $result;
+    }
+  echo json_encode([
+    "Message" => "The file has been uploaded",
+    "Status" => "OK"
+    ]);
+}else{
+  echo json_encode([
+    "Message" => "sorry",
+    "Status" => "Error"
+    ]);
+}
+
+mysqli_close($link);
+ ?>
---
+++ saveUpdateFile_Profile.php
@@ -0,0 +1,56 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+
+header("content-type:text/javascript;charset=utf-8");
+error_reporting(0);
+error_reporting(E_ERROR | E_PARSE);
+$link = mysqli_connect('localhost', 'root', '', "app_tradesman");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    
+    exit;
+}
+
+if (!$link->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $link->error);
+    exit();
+	}
+
+
+ $file = time(). "_" . basename($_FILES['image']['name']);
+print_r($_POST);
+ $tmp_name = $_FILES['image']['tmp_name'];
+ $id = $_POST['id'];
+ $name = $_POST['name'];
+if(move_uploaded_file($tmp_name,"images/profile/".$file)){
+    unlink('images/profile/'.$name);
+    $sql =  "UPDATE image_profile   SET  file_src='$file' WHERE id='$id' ";
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        
+        /* return */echo $result;
+    } else {
+        /* return */echo $result;
+    }
+  echo json_encode([
+    "Message" => "The file has been uploaded",
+    "Status" => "OK"
+    ]);
+}else{
+  echo json_encode([
+    "Message" => "sorry",
+    "Status" => "Error"
+    ]);
+}
+
+mysqli_close($link);
+ ?>
