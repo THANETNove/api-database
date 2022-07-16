16584ea71221141fca968bb8d67571dc76068dd5
THANETNove <Novel_Manga@outlook.co.th>
2022-07-16 15:07:22 +0700

    updaye บัญชีธนาคาร

---
+++ updateBank.php
@@ -0,0 +1,45 @@
+<?php
+header("content-type:text/javascript;charset=utf-8");
+error_reporting(0);
+error_reporting(E_ERROR | E_PARSE);
+$link = mysqli_connect('localhost', 'root', '', "app_tradesman");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    
+    exit;
+}
+
+if (!$link->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $link->error);
+    exit();
+	}
+
+
+	
+if (isset($_POST)) {
+    print_r($_POST);
+	if ($_POST['isAdd'] == 'true') {	
+        $id = $_POST['id'];
+		$name = $_POST['name'];
+		$number_bank = $_POST['number_bank'];
+		$bank = $_POST['bank'];
+	
+      
+		$sql = "UPDATE book_banks SET  name='$name',number_bank='$number_bank' , bank='$bank'
+         WHERE id='$id' ";
+
+		$result = mysqli_query($link, $sql);
+		if ($result) {
+			
+			/* return */echo $result;
+		} else {
+			/* return */echo $result;
+		}
+	} else echo "Welcome Master UNG";
+   
+}
+	mysqli_close($link);
+?>
