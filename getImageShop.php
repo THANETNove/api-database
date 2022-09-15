9a11614516ecd2a67ebad55f63c6efaf863468f3
THANETNove <Novel_Manga@outlook.co.th>
2022-09-16 01:21:01 +0700

    upcode

--- getImageShop.php
+++ getImageShop.php
@@ -23,7 +23,7 @@ if (isset($_GET)) {
    /*      print_r($_GET); */
 	if ($_GET['isAdd'] == 'true') {
 		$id = $_GET['id'];
-		$result = mysqli_query($link, "SELECT * FROM shop   WHERE shop.id_shop ='$id'");
+		$result = mysqli_query($link, "SELECT * FROM image_shop   WHERE image_shop.id_user ='$id'");
 		if ($result) {
 			while($row=mysqli_fetch_assoc($result)){
 			$output[]=$row;
